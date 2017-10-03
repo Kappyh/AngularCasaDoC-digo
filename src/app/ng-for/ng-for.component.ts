841c4f66ae22774572131de5914294244c46600d
Gabriela Mendes <contato@gabrielamendesp.com.br>
2017-10-02 22:00:48 -0300

    Incluindo trackBy

--- src/app/ng-for/ng-for.component.ts
+++ src/app/ng-for/ng-for.component.ts
@@ -7,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgForComponent implements OnInit {
 
-  public nomes: Array<string>;
+  public nomes: Array<any>;
   constructor() { }
 
   ngOnInit() {
-    this.nomes = ['João', 'Maria', 'Thiago', 'José'];
+    this.nomes = [
+      { id: 1, nome: 'Gabriela' },
+      { id: 2, nome: 'Maria' },
+      { id: 3, nome: 'Thiago' },
+      { id: 4, nome: 'José' }
+    ];
+  }
+
+  public meuSave(index: number, nomes: any) {
+    return nomes.id;
+  }
+
+  public atualizar() {
+    this.nomes = [
+      { id: 1, nome: 'Larissa' },
+      { id: 2, nome: 'Maria' },
+      { id: 3, nome: 'Thiago' },
+      { id: 4, nome: 'José' }
+    ];
   }
 
 }
